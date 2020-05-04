63a41816f26b930e51cddf63bcd99d6a0de8093a
JDunson98 <45635230+JDunson98@users.noreply.github.com>
2020-05-03 19:20:43 -0500

    almost finished mvp, doing more styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+
+const Character = props => {
+    return (
+                <Container>
+                    <Row>
+                        <Col sm="4">
+                            <Card>
+                                <CardImg top width="100%" src={props.image} alt="Card image cap" />
+                                <CardBody>
+                                <CardTitle><h3>{props.name}</h3></CardTitle>
+                                <CardSubtitle>Species: {props.species}</CardSubtitle>
+                                <Button>Status: {props.status}</Button>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            ) 
+}
+
+export default Character
